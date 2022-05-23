6ffb4702f5907df3932d1591ae9a034902c2ca61
MarcPIster <marc.pister@epitech.eu>
2022-05-23 06:26:58 -0700

    added cleaning script

---
+++ clean.sh
@@ -0,0 +1,9 @@
+#Script to remove trash bevor commiting
+rm Makefile
+rm cmake_install.cmake
+rm -rf CmakeFiles
+rm -rf _deps
+rm CmakeCache.txt
+rm CPackConfig.cmake
+rm CPackSourceConfig.cmake
+rm bombermann
