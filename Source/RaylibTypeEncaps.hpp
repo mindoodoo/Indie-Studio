b8f12a7621e4cf8b289ce0f1d081485332808e9c
Léon Sautouré <leon2@sautour.net>
2022-05-27 15:19:41 +0200

    Removed concept, replaced by require template statement + added utilitarian typedefs (USE THESE)

--- Source/RaylibTypeEncaps.hpp
+++ Source/RaylibTypeEncaps.hpp
@@ -6,31 +6,64 @@
 
 namespace RL {
 
-    // This defines a "concept" (c++ 20) Numerical that allows us to constrain
-    // templates to only be of numerical values
-    template<typename T>
-    concept Numerical = requires(T)
-    {
-        requires std::is_arithmetic_v<T>;
-    };
-
-    template<Numerical T>
+    template<class T>
+    requires std::is_arithmetic_v<T>
     class Vector2 {
     public:
         Vector2(T x, T y) {
-            this->x = x;
-            this->y = y;
+            this->_x = x;
+            this->_y = y;
         };
         Vector2() = default;
 
-        struct Vector2 getStruct() const {
-            return {this->x, this->y}
+        ::Vector2 getStruct() const {
+            return {this->_x, this->_y};
         }
 
-    private:
+        // Allows for passing this class as Vector2 struct without calling
+        // getStruct
+        ::Vector2 operator(::Vector2)() const {
+            return {this->_x, this->_y};
+        }
+
         T _x = 0;
         T _y = 0;
     };
+
+    template<class T>
+    requires std::is_arithmetic_v<T>
+    class Vector3 {
+    public:
+        Vector3(T x, T y, T z) {
+            this->_x = x;
+            this->_y = y;
+            this->_z = z;
+        };
+        Vector3() = default;
+
+        ::Vector3 getStruct() const {
+            return {this->_x, this->_y, this->_z};
+        }
+
+        // Allows for passing this class as Vector2 struct without calling
+        // getStruct
+        ::Vector3 operator(::Vector3)() const {
+            return {this->_x, this->_y};
+        }
+
+        T _x = 0;
+        T _y = 0;
+        T _z = 0;
+    };
+
+    // Utilitarian typedefs
+    // USE THESE FOR CLARITY
+    typedef Vector2<int> Vector2i;
+    typedef Vector3<int> Vector3i;
+    typedef Vector2<float> Vector2f;
+    typedef Vector3<float> Vector3f;
 }
 
 #endif
+
+// operator conversion
